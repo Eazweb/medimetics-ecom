34ffc8b3b75d922de10fc1a79fb6454448259acb
Riteshk_007 <codeshorts007@gmail.com>
2024-06-09 11:14:46 +0530

    chore: Update Hero component with lazy loading for images

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -2,7 +2,13 @@
 import Image from "next/image";
 import React, { useState, useEffect } from "react";
 
-const images = ["/banner/1.jpg", "/banner/2.jpg"];
+const images = [
+  "https://images.unsplash.com/photo-1483985988355-763728e1935b?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/flagged/photo-1570733117311-d990c3816c47?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1493655161922-ef98929de9d8?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1485570661444-73b3f0ff9d2f?q=80&w=1785&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1484328256245-34b71758c30b?q=80&w=2073&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+];
 const Hero = () => {
   const [currentImage, setCurrentImage] = useState(images[0]);
   const [index, setIndex] = useState(0);
@@ -39,9 +45,10 @@ const Hero = () => {
           <Image
             className="w-full rounded-xl"
             src={currentImage}
-            alt="Image Description"
+            alt="fashion"
             width={900}
             height={700}
+            loading="lazy"
             style={{ width: "100%", transition: "opacity 1s ease-in-out" }}
           />
         </div>
