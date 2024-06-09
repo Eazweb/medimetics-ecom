ed3e18c7a6a7462809ee3b7f3f7eb50766d45605
Riteshk_007 <codeshorts007@gmail.com>
2024-06-09 11:01:45 +0530

    chore: Add Category component to Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import Category from "./components/Category";
 import Hero from "./components/Hero";
 
 export default function Home() {
   return (
     <div>
       <Hero />
+      <Category />
     </div>
   );
 }
--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,10 @@ const nextConfig = {
         hostname: "images.unsplash.com",
         protocol: "https",
       },
+      {
+        hostname: "i.ibb.co",
+        protocol: "https",
+      },
     ],
   },
 };
