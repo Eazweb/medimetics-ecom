58ba12c1054cd5e557eb00123242145bd386f0a5
Riteshk_007 <codeshorts007@gmail.com>
2024-06-10 15:01:29 +0530

    chore: Update HomeProducts component to handle empty product list

--- app/components/HomeProducts.tsx
+++ app/components/HomeProducts.tsx
@@ -4,9 +4,15 @@ import { products } from "../temp/shop";
 const HomeProducts = () => {
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 p-4">
-      {products?.map((product, index) => (
-        <ProductCard key={index} product={product} />
-      ))}
+      {products && products ? (
+        products?.map((product, index) => (
+          <ProductCard key={index} product={product} />
+        ))
+      ) : (
+        <h1 className="text-2xl font-bold text-center text-gray-400">
+          No products found
+        </h1>
+      )}
     </div>
   );
 };
