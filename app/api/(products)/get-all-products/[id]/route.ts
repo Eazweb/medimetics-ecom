1a8ca394498eeb402bde12afde571dc333e306a4
Riteshk_007 <codeshorts007@gmail.com>
2024-06-13 20:29:47 +0530

    create: get one products details (data)

---
+++ app/api/(products)/get-all-products/[id]/route.ts
@@ -0,0 +1,28 @@
+import { prismaDB } from "@/db/db.config";
+import { NextRequest, NextResponse } from "next/server";
+
+export const GET = async (_: any, { params }: { params: { id: string } }) => {
+  try {
+    const product = await prismaDB.product.findUnique({
+      where: {
+        id: params?.id,
+      },
+      include: {
+        categories: {
+          select: {
+            name: true,
+          },
+        },
+      },
+    });
+    if (!product)
+      NextResponse.json({ message: "Product not found" }, { status: 404 });
+    return NextResponse.json(product, { status: 200 });
+  } catch (error) {
+    console.error("Failed to fetch product:", error);
+    return NextResponse.json(
+      { message: "An error occurred while fetching the product." },
+      { status: 500 }
+    );
+  }
+};
