138fe27c94a6741ca6e087ce752a001453002a7f
Riteshk_007 <codeshorts007@gmail.com>
2024-06-25 20:54:17 +0530

    refactor: Add Redis caching for product retrieval

--- app/api/(products)/get-all-products/[id]/route.ts
+++ app/api/(products)/get-all-products/[id]/route.ts
@@ -1,23 +1,38 @@
 import { prismaDB } from "@/db/db.config";
+import client from "@/lib/Redis";
 import { NextResponse } from "next/server";
 
 export const GET = async (_: any, { params }: { params: { id: string } }) => {
   try {
-    const product = await prismaDB.product.findUnique({
-      where: {
-        id: params?.id,
-      },
-      include: {
-        categories: {
-          select: {
-            id: true,
-            name: true,
+    
+    const cachedProduct = await client.get(`product:${params.id}`);
+    let product;
+
+    if (cachedProduct) {
+      
+      product = JSON.parse(cachedProduct);
+    } else {
+      product = await prismaDB.product.findUnique({
+        where: {
+          id: params?.id,
+        },
+        include: {
+          categories: {
+            select: {
+              id: true,
+              name: true,
+            },
           },
         },
-      },
-    });
-    if (!product)
-      NextResponse.json({ message: "Product not found" }, { status: 404 });
+      });
+
+      if (product) {
+        await client.setex(`product:${params.id}`, 120, JSON.stringify(product));
+      } else {
+        return NextResponse.json({ message: "Product not found" }, { status: 404 });
+      }
+    }
+
     return NextResponse.json(product, { status: 200 });
   } catch (error) {
     console.error("Failed to fetch product:", error);
