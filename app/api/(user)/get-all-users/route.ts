96aa980c78217f2cc47b5894065c90e2c30234da
Riteshk_007 <codeshorts007@gmail.com>
2024-06-25 20:58:31 +0530

    refactor: Add Redis caching for user retrieval

--- app/api/(user)/get-all-users/route.ts
+++ app/api/(user)/get-all-users/route.ts
@@ -1,25 +1,34 @@
 import { prismaDB } from "@/db/db.config";
+import client from "@/lib/Redis";
 import { NextResponse } from "next/server";
-
 export const GET = async (_: any) => {
   try {
-    const Users = await prismaDB.user.findMany({
-      select: {
-        id: true,
-        name: true,
-        email: true,
-        password: false,
-        isAdmin: true,
-        createdAt: true,
-        updatedAt: true,
-      },
-    });
-    if (!Users)
-      return NextResponse.json({ message: "No Users Found", status: 404 });
-    if (Users && Users.length === 0)
-      return NextResponse.json({ message: "No Users Found", status: 404 });
+    const cachedUsers = await client.get("users");
+    let Users;
+
+    if (cachedUsers) {
+      Users = JSON.parse(cachedUsers);
+    } else {
+      Users = await prismaDB.user.findMany({
+        select: {
+          id: true,
+          name: true,
+          email: true,
+          isAdmin: true,
+          createdAt: true,
+          updatedAt: true,
+        },
+      });
+
+      if (Users && Users.length > 0) {
+        await client.setex("users", 300, JSON.stringify(Users));
+      } else if (Users.length === 0) {
+        return NextResponse.json({ message: "No Users Found", status: 404 });
+      }
+    }
     return NextResponse.json(Users, { status: 200 });
   } catch (error) {
+    console.error("Failed to fetch users:", error);
     return NextResponse.json({ message: "Internal Server Error", status: 500 });
   }
 };
