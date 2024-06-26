f3e9c6feb2ee2c5464a417c49f250cba43ab742b
Riteshk_007 <codeshorts007@gmail.com>
2024-06-26 17:22:38 +0530

    refactor: Update Redis configuration for improved caching and error handling

--- lib/Redis.ts
+++ lib/Redis.ts
@@ -1,23 +1,16 @@
 import { Redis } from "ioredis";
 
-const redisUrl = process.env.REDIS || "";
-if (!redisUrl) {
-  console.error("REDIS environment variable is not set.");
+export const client = new Redis(process.env.REDIS || "redis://localhost:6379", {
+  tls: {
+    rejectUnauthorized: false,
+  },
+});
+if (process.env.REDIS) {
+  client.on("connect", () => {
+    console.log("Redis client connected");
+  });
+
+  client.on("error", (error) => {
+    console.error("Redis client error:", error);
+  });
 }
-
-export const client = new Redis(redisUrl, {
-  tls:
-    process.env.NODE_ENV === "production"
-      ? {
-          rejectUnauthorized: true,
-        }
-      : undefined,
-});
-
-client.on("connect", () => {
-  console.log("Redis client connected");
-});
-
-client.on("error", (error) => {
-  console.error("Redis client error:", error);
-});
