5aa935aa41ea97c78530d728ca19ed532e6c086b
Riteshk_007 <codeshorts007@gmail.com>
2024-06-26 20:14:35 +0530

    refactor: Update API base URL in GetAllProductsSlice and GetAllUserSlice

--- providers/toolkit/features/GetAllProductsSlice.ts
+++ providers/toolkit/features/GetAllProductsSlice.ts
@@ -14,7 +14,7 @@ type Product = {
 
 const baseUrl =
   process.env.NODE_ENV === "production"
-    ? "https://shop-smart-lilac.vercel.app/api"
+    ? "https://shop-smart-lilac.vercel.app/api/"
     : "http://localhost:3000/api/";
 
 export const productsApi = createApi({
