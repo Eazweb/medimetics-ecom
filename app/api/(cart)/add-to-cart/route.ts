73cd9d58543e240ebad15fed70ad120cb0bf84d0
Riteshk_007 <codeshorts007@gmail.com>
2024-06-28 12:58:28 +0530

    refactor: Add color and size parameters to add-to-cart API route

--- app/api/(cart)/add-to-cart/route.ts
+++ app/api/(cart)/add-to-cart/route.ts
@@ -2,90 +2,88 @@ import { prismaDB } from "@/db/db.config";
 import { NextRequest, NextResponse } from "next/server";
 
 export const POST = async (req: NextRequest) => {
-  const { userId, productId, quantity } = await req.json();
   try {
-    if (!userId) {
-      return NextResponse.json({ error: "User ID is required", status: 400 });
-    }
-    if (!productId) {
-      return NextResponse.json({
-        error: "Product ID is required",
-        status: 400,
-      });
+    const { userId, productId, quantity, color, size } = await req.json();
+
+    if (!userId || !productId) {
+      return new NextResponse(
+        JSON.stringify({
+          message: "User ID and Product ID are required",
+        }),
+        { status: 400 }
+      );
     }
 
-    // Check if the user exists
     const userExists = await prismaDB.user.findUnique({
-      where: {
-        id: userId,
-      },
+      where: { id: userId },
     });
     if (!userExists) {
-      return NextResponse.json({
-        error: "User not found",
-        status: 404,
-      });
+      return new NextResponse(
+        JSON.stringify({
+          message: "User not found",
+        }),
+        { status: 404 }
+      );
     }
 
-    // Check if the product exists
     const productExists = await prismaDB.product.findUnique({
-      where: {
-        id: productId,
-      },
+      where: { id: productId },
     });
     if (!productExists) {
-      return NextResponse.json({
-        error: "Product not found",
-        status: 404,
-      });
+      return new NextResponse(
+        JSON.stringify({
+          message: "Product not found",
+        }),
+        { status: 404 }
+      );
     }
 
-    // Check if the cart item already exists for the user and product
     const existingCartItem = await prismaDB.cart.findFirst({
       where: {
         userId,
         productId,
+        color,
+        size,
       },
     });
 
     if (existingCartItem) {
-      // Calculate the new quantity
-      const newQuantity = existingCartItem.quantity + quantity;
-
-      // If exists, update the quantity
       const updatedCartItem = await prismaDB.cart.update({
-        where: {
-          id: existingCartItem.id,
-        },
-        data: {
-          quantity: newQuantity,
-        },
+        where: { id: existingCartItem.id },
+        data: { quantity: existingCartItem.quantity + quantity },
       });
-      return NextResponse.json({
-        message: "Cart item quantity updated",
-        status: 200,
-        data: updatedCartItem,
-      });
+      return new NextResponse(
+        JSON.stringify({
+          message: "Cart item updated with new quantity",
+          data: updatedCartItem,
+        }),
+        { status: 200 }
+      );
     } else {
-      // If not, create a new cart item
       const newCartItem = await prismaDB.cart.create({
         data: {
           userId,
           productId,
           quantity,
+          color,
+          size,
         },
       });
-      return NextResponse.json({
-        message: "Product added to cart",
-        status: 201,
-        data: newCartItem,
-      });
+      return new NextResponse(
+        JSON.stringify({
+          message: "New product added to cart",
+          data: newCartItem,
+        }),
+        { status: 201 }
+      );
     }
   } catch (error) {
     console.error(error);
-    return NextResponse.json({
-      error: "Something went wrong while adding product to cart",
-      status: 500,
-    });
+    return new NextResponse(
+      JSON.stringify({
+        message: "An error occurred while processing your request",
+      }),
+      { status: 500 }
+    );
   }
 };
