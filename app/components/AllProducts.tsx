f6c9adda6d48eec58a4a1fc6184577b82a62fb3f
Riteshk_007 <codeshorts007@gmail.com>
2024-07-02 17:42:47 +0530

    refactor: Update AllProducts component to use AllProduct component

---
+++ app/components/AllProducts.tsx
@@ -0,0 +1,98 @@
+"use client";
+import { useState } from "react";
+import ProductCard from "@/app/components/ProductCard";
+
+import { Grid, List } from "lucide-react";
+import { useGetAllProductsQuery } from "@/providers/toolkit/features/GetAllProductsSlice";
+import ProductsSkeletons from "@/app/temp/ProductsSkeletons";
+
+const AllProduct = () => {
+  const [viewMode, setViewMode] = useState<"grid" | "list">("grid");
+  const [sortOption, setSortOption] = useState<string>("1");
+  const { data, error, isLoading } = useGetAllProductsQuery();
+
+  type Product = {
+    id: number;
+    name: string;
+    price: number;
+    mainImage: string;
+    otherImages: string;
+  };
+  const sortedProducts = data
+    ? [...data].sort((a, b) => {
+        switch (sortOption) {
+          case "2":
+            return a.price - b.price;
+          case "3":
+            return b.price - a.price;
+          case "4":
+            return a.name.localeCompare(b.name);
+          case "5":
+            return b.name.localeCompare(a.name);
+          default:
+            return 0;
+        }
+      })
+    : [];
+
+  if (isLoading) {
+    return <ProductsSkeletons />;
+  }
+  return (
+    <div className="w-full flex flex-col">
+      <div className="flex w-full items-center justify-between p-4 bg-gray-100 rounded-lg shadow z-50 sticky top-0">
+        <div className="flex items-center">
+          <span className="ml-2 font-semibold text-gray-800">All Products</span>
+        </div>
+        <div className="flex gap-3 items-center">
+          <Grid
+            size={24}
+            className={`text-gray-600 hover:text-gray-500 cursor-pointer ${
+              viewMode === "grid"
+                ? "bg-gray-800 text-white p-[3px] rounded"
+                : ""
+            }`}
+            onClick={() => setViewMode("grid")}
+          />
+          <List
+            size={24}
+            className={`text-gray-600 hover:text-gray-500 cursor-pointer ${
+              viewMode === "list"
+                ? "bg-gray-800 text-white p-[3px] rounded"
+                : ""
+            }`}
+            onClick={() => setViewMode("list")}
+          />
+          <select
+            className="p-2 bg-white border border-gray-300 rounded-md text-gray-700 hover:border-gray-400 focus:ring-blue-500 focus:border-blue-500"
+            value={sortOption}
+            onChange={(e) => setSortOption(e.target.value)}
+          >
+            <option value="1">Sort by</option>
+            <option value="2">Price: Low to High</option>
+            <option value="3">Price: High to Low</option>
+            <option value="4">Name: A to Z</option>
+            <option value="5">Name: Z to A</option>
+          </select>
+        </div>
+      </div>
+      <div
+        className={`p-4 z-30 ${
+          viewMode === "grid"
+            ? "grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4"
+            : "grid grid-cols-2 gap-4"
+        }`}
+      >
+        {error ? (
+          <div>Error loading products.</div>
+        ) : (
+          sortedProducts.map((product, index) => (
+            <ProductCard key={index} product={product as unknown as Product} />
+          ))
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default AllProduct;
