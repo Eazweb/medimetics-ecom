00d0e280e9cbd65d50a49801ec0445e94f5ac4c8
Riteshk_007 <codeshorts007@gmail.com>
2024-07-02 20:48:44 +0530

    refactor: Add lazy loading for images in ToastCartButton component

--- app/components/ToastCartButton.tsx
+++ app/components/ToastCartButton.tsx
@@ -1,14 +1,23 @@
 "use client";
 import { useToast } from "@/components/ui/use-toast";
+import { AddToCart } from "@/providers/toolkit/features/AddToCartSlice";
+import { useAppDispatch } from "@/providers/toolkit/hooks/hooks";
 import { ShoppingCart } from "lucide-react";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 
+interface SessionUser {
+  id: string;
+}
 const ToastCartButton = ({ product }: any) => {
   const { data: session } = useSession();
   const { push } = useRouter();
   const { toast } = useToast();
-  const handleClick = () => {
+  const dispatch = useAppDispatch();
+  const [isLoading, setIsLoading] = useState(false);
+  const handleClick = async () => {
+    setIsLoading(true);
     if (!session?.user) {
       toast({
         title: "Please login to add to cart",
@@ -20,27 +29,40 @@ const ToastCartButton = ({ product }: any) => {
           color: "#fff",
         },
       });
+      setIsLoading(false);
       return push("/login");
     } else {
-      toast({
-        title: "Added to Cart",
-        description: `${product.name} was added to your cart`,
-        variant: "default",
-        duration: 1500,
-        style: {
-          backgroundColor: "#191919",
-          color: "#fff",
-        },
-      });
+      const userId = (session.user as SessionUser).id;
+      await dispatch(
+        AddToCart({
+          userId: userId,
+          productId: product.id,
+          quantity: 1,
+          color: product.colors[0],
+          size: product.sizes[0],
+        })
+      );
+      setIsLoading(false);
     }
   };
   return (
     <button
       onClick={handleClick}
       className="mt-4 py-2 px-4 bg-black text-white rounded-lg flex items-center justify-center hover:bg-gray-900  sm:w-auto"
+      disabled={isLoading}
     >
-      <ShoppingCart className="w-6 h-6 sm:w-5 sm:h-5 md:mr-2" />
-      <span className="hidden sm:block">Add to Cart</span>
+      {isLoading ? (
+        <>
+          <div className="cursor-wait" role="status">
+            <span className="visually-hidden">Loading...</span>
+          </div>
+        </>
+      ) : (
+        <>
+          <ShoppingCart className="w-6 h-6 sm:w-5 sm:h-5 md:mr-2" />
+          <span className="hidden sm:block">Add to Cart</span>
+        </>
+      )}
     </button>
   );
 };
