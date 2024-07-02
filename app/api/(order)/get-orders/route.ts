99ab9b7261763589f557dbde0e84957de8268240
Riteshk_007 <codeshorts007@gmail.com>
2024-07-02 14:30:27 +0530

    Create: get User All Orders

---
+++ app/api/(order)/get-orders/route.ts
@@ -0,0 +1,34 @@
+import { prismaDB } from "@/db/db.config";
+import { NextRequest, NextResponse } from "next/server";
+
+export const POST = async (req: NextRequest) => {
+  const { userId } = await req.json();
+  try {
+    if (!userId) {
+      return NextResponse.json(
+        { message: "User id is required" },
+        { status: 400 }
+      );
+    }
+    const orders = await prismaDB.order.findMany({
+      where: {
+        usersId: userId,
+      },
+      include: {
+        Product: {
+          select: {
+            name: true,
+            price: true,
+            mainImage: true,
+          },
+        },
+      },
+    });
+    return NextResponse.json({ orders, status: 200 });
+  } catch (error) {
+    return NextResponse.json(
+      { message: "Error fetching orders" },
+      { status: 500 }
+    );
+  }
+};
