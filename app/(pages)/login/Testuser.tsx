023efbe415bc259b468d9dc12a2866b79426f58a
Riteshk_007 <codeshorts007@gmail.com>
2024-07-17 20:44:37 +0530

    refactor: Update TestUser component for login page

--- app/(pages)/login/Testuser.tsx
+++ app/(pages)/login/Testuser.tsx
@@ -1,11 +1,15 @@
 import { useToast } from "@/components/ui/use-toast";
 import React from "react";
-import { FiCopy } from "react-icons/fi";
+import { CopyIcon } from "lucide-react";
 
 const TestUser = () => {
   const { toast } = useToast();
-  const userInfo = {
-    email: "rk0001945@gmail.com",
+  const AdminInfo = {
+    email: "admin@gmail.com",
+    password: "Password123@",
+  };
+  const UserInfo = {
+    email: "user@gmail.com",
     password: "Password123@",
   };
 
@@ -23,24 +27,57 @@ const TestUser = () => {
   };
 
   return (
-    <div className="flex flex-col mt-5 space-y-4 p-4 max-w-sm mx-auto bg-black/20 rounded-lg shadow-md">
-      <div className="flex items-center justify-between">
-        <span className="text-gray-900 font-medium">
-          Login ID: {userInfo.email}
-        </span>
-        <FiCopy
-          className="cursor-pointer text-gray-600 hover:text-gray-900"
-          onClick={() => copyToClipboard(userInfo.email)}
-        />
+    <div className="flex flex-col space-y-6 p-4 max-w-md mx-auto">
+      <div className="p-4 bg-blue-100 rounded-lg">
+        <h2 className="text-base font-semibold text-blue-800">
+          Admin Test Account
+        </h2>
+        <div className="mt-2 text-xs">
+          <div className="flex items-center justify-between">
+            <span className="text-gray-900">Email: {AdminInfo.email}</span>
+            <CopyIcon
+              size={15}
+              className="cursor-pointer  text-gray-600 hover:text-gray-900"
+              onClick={() => copyToClipboard(AdminInfo.email)}
+            />
+          </div>
+          <div className="flex items-center justify-between mt-2">
+            <span className="text-gray-900">
+              Password: {"*".repeat(AdminInfo.password.length)}
+            </span>
+            <CopyIcon
+              size={15}
+              className="cursor-pointer  text-gray-600 hover:text-gray-900"
+              onClick={() => copyToClipboard(AdminInfo.password)}
+            />
+          </div>
+        </div>
       </div>
-      <div className="flex items-center justify-between">
-        <span className="text-gray-900 font-medium">
-          Password: {"*".repeat(userInfo.password.length)}
-        </span>
-        <FiCopy
-          className="cursor-pointer text-gray-600 hover:text-gray-900"
-          onClick={() => copyToClipboard(userInfo.password)}
-        />
+
+      <div className="p-4 bg-green-100 rounded-lg">
+        <h2 className="text-base font-semibold text-green-800">
+          User Test Account
+        </h2>
+        <div className="mt-2 text-xs">
+          <div className="flex items-center justify-between">
+            <span className="text-gray-900">Login ID: {UserInfo.email}</span>
+            <CopyIcon
+              size={15}
+              className="cursor-pointer  text-gray-600 hover:text-gray-900"
+              onClick={() => copyToClipboard(UserInfo.email)}
+            />
+          </div>
+          <div className="flex items-center justify-between mt-2">
+            <span className="text-gray-900">
+              Password: {"*".repeat(UserInfo.password.length)}
+            </span>
+            <CopyIcon
+              size={15}
+              className="cursor-pointer  text-gray-600 hover:text-gray-900"
+              onClick={() => copyToClipboard(UserInfo.password)}
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
